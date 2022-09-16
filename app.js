ef973fa20cdaa3861da5486f5f9896b3b304549e
gal gordon <gal.gordon@gmail.com>
2022-09-16 09:57:01 -0300

    fixed for heruko

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var path = require('path');
 
 var indexRouter = require('./routes/index');
 
@@ -12,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
